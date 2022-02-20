8d8aac0b5560761f4d98e36680e89541f8ce860b
Nero Van gestel <nvan-ges@shi-r4-p4.s19.be>
2022-02-20 15:39:11 +0100

    1.2

--- c__05/ex00/ft_iterative_factorial.c
+++ c__05/ex00/ft_iterative_factorial.c
@@ -6,23 +6,25 @@
 /*   By: nvan-ges <nvan-ges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:38:57 by nvan-ges          #+#    #+#             */
-/*   Updated: 2022/02/20 13:27:27 by nvan-ges         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:38:49 by nvan-ges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
 	int	i;
+	int x;
 
 	i = 1;
+	x = nb;
 	if (nb == 0)
 		return (1);
 	if (nb < 0)
 		return (0);
-	while (i < 5)
+	while (i < nb)
 	{
-		nb *= i;
+		x *= i;
 		i++;
 	}
-	return (nb);
+	return (x);
 }
